6d81693252120497ba11102d7f7dc1e54e163cb5
Guo Shiwei <guoshiwei@gmail.com>
2012-10-25 10:22:33 +0800

    Move custom-set-variables command from common.el to custom.el

--- personal/custom.el
+++ personal/custom.el
@@ -3,6 +3,34 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("D:/Dropbox/org/todo.org")))
+ '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(column-number-mode t)
+ '(custom-enabled-themes (quote (tango-dark)))
+ '(fci-rule-color "#383838")
+ '(org-agenda-custom-commands (quote (("d" todo "DELEGATED" nil) ("c" todo "DONE|DEFERRED|CANCELLED" nil) ("w" todo "WAITING" nil) ("W" agenda "" ((org-agenda-ndays 21))) ("A" agenda "" ((org-agenda-skip-function (lambda nil (org-agenda-skip-entry-if (quote notregexp) "\\=.*\\[#A\\]"))) (org-agenda-ndays 1) (org-agenda-overriding-header "Today's Priority #A tasks: "))) ("u" alltodo "" ((org-agenda-skip-function (lambda nil (org-agenda-skip-entry-if (quote scheduled) (quote deadline) (quote regexp) " ]+>"))) (org-agenda-overriding-header "Unscheduled TODO entries: "))))))
+ '(org-agenda-files (quote ("~/Dropbox/org/todo.org")))
+ '(org-agenda-ndays 7)
+ '(org-agenda-show-all-dates t)
+ '(org-agenda-skip-deadline-if-done t)
+ '(org-agenda-skip-scheduled-if-done t)
+ '(org-agenda-start-on-weekday nil)
+ '(org-deadline-warning-days 14)
  '(org-default-notes-file "D:/Dropbox/org/notes.org")
- '(org-remember-templates (quote ((116 "* %?  %u" "D:/Dropbox/org/todo.org" "Tasks") (110 "* %u %?" "D:/Dropbox/org/notes.org" "Notes")))))
+ '(org-fast-tag-selection-single-key (quote expert))
+ '(org-hide-leading-stars t)
+ '(org-log-into-drawe t)
+ '(org-odd-levels-only t)
+ '(org-remember-store-without-prompt t)
+ '(org-remember-templates (quote ((116 "* %?  %u" "D:/Dropbox/org/todo.org" "Tasks") (110 "* %u %?" "D:/Dropbox/org/notes.org" "Notes"))))
+ '(org-reverse-note-order t)
+ '(remember-annotation-functions (quote (org-remember-annotation)))
+ '(remember-handler-functions (quote (org-remember-handler)))
+ '(show-paren-mode t)
+ '(size-indication-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "FixedsysTTF" :foundry "unknown" :slant normal :weight semi-bold :height 113 :width normal)))))
