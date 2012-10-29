0ef33cf33739991abdfc69c4036d873bc5d6fa57
Guo Shiwei <guoshiwei@gmail.com>
2012-10-29 20:29:15 +0800

    Use Fixsys font.

--- personal/custom.el
+++ personal/custom.el
@@ -27,10 +27,11 @@
  '(remember-handler-functions (quote (org-remember-handler)))
  '(show-paren-mode t)
  '(size-indication-mode t)
- '(tool-bar-mode nil))
+ '(tool-bar-mode nil)
+ '(transient-mark-mode (quote (only . t))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "FixedsysTTF" :foundry "unknown" :slant normal :weight semi-bold :height 113 :width normal)))))
+ '(default ((t (:family "Fixedsys Excelsior 3.01" :foundry "unknown" :slant normal :weight normal :height 120 :width normal)))))
